21b97eded77572cbd41e939679b5e503b0f0f6b2
dming <dming@Dalton-HPEnvyx360>
2017-03-17 13:41:21 -0400

    Changed from LinkedList implementation to Node implementation

--- src/dming98/Processes/Queue.java
+++ src/dming98/Processes/Queue.java
@@ -5,58 +5,104 @@ import java.util.LinkedList;
 public class Queue {
 	int maxSize;
 	int currentSize;
-	LinkedList<Process> elements = new LinkedList<Process>();
+
+	public class Node {
+		Process item;
+		Node next;
+
+		public Node(Process value) {
+			item = value;
+		}
+
+		public Node(Process value, Node next) {
+			item = value;
+			this.next = next;
+		}
+	}
+
+	private Node head;
+	private Node tail;
 
 	public Queue() {
 		this.maxSize = 50;
 		currentSize = 0;
+		head = null;
+		tail = null;
 	}
 
 	public Queue(int maxSize) {
 		if (maxSize <= 50) {
 			this.maxSize = maxSize;
 			currentSize = 0;
+			head = null;
+			tail = null;
 		} else
 			throw new IllegalArgumentException("The max size cannot be greater than 50.");
 	}
 
-	public int getCurrentSize(){
+	public int getCurrentSize() {
 		return currentSize;
 	}
+
 	public void priorityEnqueue(Process newProcess) {
+		Node input = new Node(newProcess);
 		if (isEmpty()) {
-			elements.add(newProcess);
+			head = input;
+			tail = input;
 			currentSize++;
 		} else if (!isFull()) {
-			boolean changed = false;
-			for (int i = 0; i < currentSize; i++) {
-				Process current = elements.get(i);
-				if ((newProcess.getBurstTime() < current.getBurstTime()) && !changed) {
-					elements.add(i, newProcess);
-					changed = true;
-					currentSize++;
-				}
-			}
-			if (!changed) {
-				elements.add(currentSize, newProcess);
-				changed = true;
+			if (head.item.getBurstTime() > input.item.getBurstTime()) {
+				input.next = head;
+				head = input;
 				currentSize++;
+			} else if (tail.item.getBurstTime() < input.item.getBurstTime()) {
+				tail.next = input;
+				tail = input;
+				currentSize++;
+			} else {
+				Node curr = head.next;
+				Node prev = head;
+				while (curr.next != null) {
+					if (curr.item.getBurstTime() > input.item.getBurstTime()) {
+						input.next = curr;
+						prev.next = input;
+						currentSize++;
+					}
+					curr = curr.next;
+					prev = prev.next;
+
+				}
 			}
 		} else
 			throw new IndexOutOfBoundsException("The queue is full.");
 	}
 
 	public void enqueue(Process newProcess) {
-		if (!isFull()) {
-			elements.add(newProcess);
+		Node input = new Node(newProcess);
+		if (isEmpty()) {
+			head = input;
+			tail = input;
 			currentSize++;
-		}
+		} else if (!isFull()) {
+			tail.next = input;
+			tail = input;
+			currentSize++;
+		} else
+			throw new IndexOutOfBoundsException("The queue is full.");
 	}
 
 	public Process dequeue() {
-		Process top = elements.remove();
-		currentSize--;
-		return top;
+		Process begin = null;
+		if (!isEmpty()) {
+			begin = head.item;
+			head = head.next;
+			currentSize--;
+			if (currentSize == 0) {
+				tail = null;
+			}
+		} else
+			throw new IndexOutOfBoundsException("The queue is empty.");
+		return begin;
 	}
 
 	public boolean isFull() {
@@ -72,12 +118,12 @@ public class Queue {
 	}
 
 	public void printQueue() {
-		int i = 0;
 		System.out.printf("ID\tBurst Time\n");
-		while (i < currentSize) {
-			Process current = elements.get(i);
+		Node curr = head;
+		while (curr.next != null) {
+			Process current = curr.item;
 			System.out.printf("%d\t%f\n", current.getID(), current.getBurstTime());
-			i++;
+			curr = curr.next;
 		}
 	}
 }

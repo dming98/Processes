4c54354c6da4d098780615e64a04ae4e76b71e81
dming <dming@Dalton-HPEnvyx360>
2017-03-20 13:22:56 -0400

    Added calculations for average times.

--- tests/dming98/Processes/OSSimulator.java
+++ tests/dming98/Processes/OSSimulator.java
@@ -1,24 +1,40 @@
 package dming98.Processes;
 
+
 import java.io.*;
 import java.util.*;
 
 public class OSSimulator {
 	public static void main(String[] args) throws FileNotFoundException {
-		String fileName;
+		String fileName, type;
 		Scanner console = new Scanner(System.in);
 
 		System.out.print("Please input the name of the file:  ");
 		fileName = console.next();
-		System.out.print("Type regular for FIFO, or type priority for priority enqueue:   ");
-		String type = console.next();
-		Queue elements = queueMaker(fileName, type);
-		elements.printQueue();
-		calculateTime(elements, type);
+
+		type = "regular";
+		Queue elementsRegular = queueMaker(fileName, type);
+		elementsRegular.printQueue();
+		calculateTime(elementsRegular, type);
+
+		type = "priority";
+		Queue elementsPriority = queueMaker(fileName, type);
+		elementsPriority.printQueue();
+		calculateTime(elementsPriority, type);
 	}
 
+	/**
+	 * Makes the queue of processes
+	 * 
+	 * @param fileName
+	 *            file to be read from
+	 * @param type
+	 *            type of enqueue to use
+	 * @return Queue of values in file
+	 * @throws FileNotFoundException
+	 */
 	public static Queue queueMaker(String fileName, String type) throws FileNotFoundException {
-		Queue elements = new Queue(50);
+		Queue elements = new Queue(15);
 		Scanner file = null;
 		FileReader in = null;
 		Scanner console = new Scanner(System.in);
@@ -37,6 +53,7 @@ public class OSSimulator {
 				Process newProcess = new Process(ID, burst);
 				elements.enqueue(newProcess);
 			}
+			System.out.println("REGULAR ENQUEUE");
 		} else if (type.trim().equalsIgnoreCase("priority")) {
 			while (file.hasNext()) {
 				int ID = file.nextInt();
@@ -44,52 +61,74 @@ public class OSSimulator {
 				Process newProcess = new Process(ID, burst);
 				elements.priorityEnqueue(newProcess);
 			}
+			System.out.println("PRIORITY ENQUEUE");
 		} else
 			throw new IllegalArgumentException("Invalid type choice.");
 		return elements;
 	}
 
-	public static void calculateTime(Queue elements, String type){
+	/**
+	 * Calculates the wait times and turn around times for a process queue.
+	 * 
+	 * @param elements
+	 *            queue to be used
+	 * @param type
+	 *            type of enqueue to use
+	 */
+	public static void calculateTime(Queue elements, String type) {
 		Process current;
-		int clock=0;
+		double totalWait = 0.0;
+		double totalTurn = 0.0;
+		int completed = 0;
+		double clock = 0.0;
 		System.out.printf("\nID\tWait Time\tEnd Time\n");
-		
-		if(type.trim().equalsIgnoreCase("regular")){
-			while(!elements.isEmpty()){
-				current=elements.dequeue();
-				if(current.getBurstRemain()<=(0.1)){
-					current.setWaitTime(current.getWaitTime()+(clock-current.getEndTime()));
-					clock=clock + (int) (1000*current.getBurstRemain());
+
+		if (type.trim().equalsIgnoreCase("regular")) {
+			while (!elements.isEmpty()) {
+				current = elements.dequeue();
+				if (current.getBurstRemain() <= (0.1)) {
+					current.setWaitTime(current.getWaitTime() + (clock - current.getEndTime()));
+					clock = clock + 1000 * current.getBurstRemain();
 					current.setEndTime(clock);
 					current.setBurstRemain(0);
 					System.out.printf("%d\t%f\t%f\n", current.getID(), current.getWaitTime(), current.getEndTime());
+					completed++;
+					totalWait += current.getWaitTime();
+					totalTurn += current.getEndTime();
 				} else {
-					current.setWaitTime(current.getWaitTime()+(clock-current.getEndTime()));
-					clock+= 100;
+					current.setWaitTime(current.getWaitTime() + (clock - current.getEndTime()));
+					clock += 100;
 					current.setEndTime(clock);
-					current.setBurstRemain(current.getBurstRemain()-.1);
+					current.setBurstRemain(current.getBurstRemain() - .1);
 					elements.enqueue(current);
 				}
 			}
-	 
-		} else if(type.trim().equalsIgnoreCase("priority")){
-			while(!elements.isEmpty()){
-				current=elements.dequeue();
-				if(current.getBurstRemain()<=(0.1)){
-					current.setWaitTime(current.getWaitTime()+(clock-current.getEndTime()));
-					clock=clock + (int) (1000*current.getBurstRemain());
+
+		} else if (type.trim().equalsIgnoreCase("priority")) {
+			while (!elements.isEmpty()) {
+				current = elements.dequeue();
+				if (current.getBurstRemain() <= (0.1)) {
+					current.setWaitTime(current.getWaitTime() + (clock - current.getEndTime()));
+					clock = clock + 1000 * current.getBurstRemain();
 					current.setEndTime(clock);
 					current.setBurstRemain(0);
 					System.out.printf("%d\t%f\t%f\n", current.getID(), current.getWaitTime(), current.getEndTime());
+					completed++;
+					totalWait += current.getWaitTime();
+					totalTurn += current.getEndTime();
 				} else {
-					current.setWaitTime(current.getWaitTime()+(clock-current.getEndTime()));
-					clock+= 100;
+					current.setWaitTime(current.getWaitTime() + (clock - current.getEndTime()));
+					clock += 100;
 					current.setEndTime(clock);
-					current.setBurstRemain(current.getBurstRemain()-.1);
+					current.setBurstRemain(current.getBurstRemain() - .1);
 					elements.priorityEnqueue(current);
 				}
 			}
+
 		} else
 			throw new IllegalArgumentException("Invalid type choice.");
+		System.out.println("The total execution time was: " + clock + " seconds.");
+		System.out.print("The average wait time was: " + (totalWait / completed) + " seconds.\n");
+		System.out.print("The average turnaround time was: " + (totalTurn / completed) + " seconds.\n\n");
 	}
 }
